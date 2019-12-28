90abdb867e54670176a2ae859afb0d4420cd2220
Skruggle <seanc893@icloud.com>
2019-12-28 15:14:28 -0600

    updated portfolio

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Portfolio
-An ever evolving portfolio over time. Learning everyday non-stop. It has become a passion, and an obsession.
+An ever evolving portfolio. Learning everyday non-stop. It has become a passion, and an obsession.
---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE php>
+
+<html>
+<form action="6.php" method="post">
+    Name: <input type="text" name="name"> <br>
+    Age: <input type="number" name="age"> <br>
+Password: <input type="password" name="password"> <br>
+<input type="submit">
+
+<br> <br>
+
+<?php funtion putin(){
+
+echo "name", "age", "password";
+
+
+} ?>
+
+
+</form>
+</html>
